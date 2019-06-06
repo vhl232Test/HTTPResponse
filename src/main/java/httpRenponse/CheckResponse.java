d77eb878ed60eb7a224d0fc52ff861ce9705ad47
vhl232Guru <vhl232@gmail.com>
2019-06-06 12:14:16 +0300

    разбор html получение -- title, meta-description, text from H1

--- src/main/java/httpRenponse/CheckResponse.java
+++ src/main/java/httpRenponse/CheckResponse.java
@@ -1,18 +1,53 @@
 package httpRenponse;
 
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+import java.io.IOException;
+
 public class CheckResponse {
     public static void main(String[] args) {
 
         String url = "http://github.com/";
-        String url200 = "https://github.com/";
-        String url400 = "https://github.com/testlololo";
+
         Util util = new Util();
+        HtmlElement htmlElement;
+        WebClient webClient = new WebClient();
 
-
+        webClient.getOptions().setCssEnabled(false);// enable css
+        webClient.getOptions().setJavaScriptEnabled(false);//enable JavaScript
 
         System.out.println( util.response(url));
-        System.out.println(util.response(url200));
-        System.out.println(util.response(url400));
+
+        if(util.response(url)>=200 || util.response(url) <300){
+            try {
+                HtmlPage htmlPage = webClient.getPage(url);//set url
+                //get title
+                String title = htmlPage.getTitleText();
+                System.out.println(title);
+
+                //get meta-description
+                htmlElement = htmlPage.getFirstByXPath("//meta[@name = 'description']");
+                String mataDescrip = htmlElement.getAttribute("content");
+                System.out.println(mataDescrip);
+
+
+                // get text from H1
+                htmlElement =  htmlPage.getFirstByXPath("//h1[@class = 'h000-mktg text-white lh-condensed-ultra mb-3']");
+                String textFromH1 = htmlElement.getTextContent();
+                System.out.println(textFromH1);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else if (util.response(url)>= 300 || util.response(url)<400){
+            System.out.println("request is redirect ");
+        }
+        else if (util.response(url)>=400){
+            System.out.println();
+        }
 
     }
 
